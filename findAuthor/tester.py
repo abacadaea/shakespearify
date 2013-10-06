a898e6929a2c4a28095062cbee1a2b77f49970db
Ray Li <rayli@Rays-MacBook-Air.local>
2013-10-05 20:58:53 -0400

    partial tester

--- findAuthor/tester.py
+++ findAuthor/tester.py
@@ -1,1 +1,21 @@
-print "Hi"
+authors = [
+'shakespeare', 
+'hi',
+]
+
+MAX_LINES_PER_FILE = 30000
+#files = dict ()
+sentences = []
+
+#open files
+#read 
+for author in authors:
+	#files [author] = open ('../data/' + author,'r')
+	wordfile = open ('../data/' + author,'r')
+	numlines = 0
+
+	for line in wordfile:
+		cur_words = lines.split ()
+		for word in cur_words:
+			if (word [len (word) - 1] == '.'):
+
