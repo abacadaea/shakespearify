77e78cd21e44df052607adabd423403c28ed6bf9
Matthew Daiter <mdaiter@exeter.edu>
2013-10-06 04:08:56 -0400

    Updated web

--- web/__init__.py
+++ web/__init__.py
@@ -1,3 +1,4 @@
+import os
 import cherrypy
 from mako.template import Template
 from mako.lookup import TemplateLookup
@@ -7,12 +8,35 @@ class Main:
   def index(self):
     tmpl = lookup.get_template("index.txt")
     return tmpl.render()
+  
+  index.exposed = True
+  
   def analyze(self, sentenceReceived=None):
     print sentenceReceived
     tmpl = lookup.get_template("index.txt")
     return tmpl.render()
 
-  index.exposed = True
   analyze.exposed=True
 
-cherrypy.quickstart(Main())
+current_dir = os.path.dirname(os.path.abspath(__file__)) + os.path.sep
+
+config = {
+        'global': {
+          'environment': 'production',
+          'log.screen': True,
+          'server.socket_host': '127.0.0.1',
+          'server.socket_port': 2000,
+          'engine.autoreload_on': True,
+          'log.error_file': os.path.join(current_dir, 'errors.log'),
+          'log.access_file': os.path.join(current_dir, 'access.log'),
+        },
+        '/':{
+            'tools.staticdir.root' : current_dir,
+          },
+          '/static':{
+              'tools.staticdir.on' : True,
+              'tools.staticdir.dir' : 'static',
+    },
+}
+
+cherrypy.quickstart(Main(), '/', config)
